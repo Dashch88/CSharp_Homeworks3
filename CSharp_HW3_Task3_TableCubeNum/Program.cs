cf4ccb58fed4e9d7a9e2ef744048db308a26ecab
Dashch <dashch88@gmail.com>
2022-11-25 17:15:47 +0400

    Added 3d task. Table of cubes of numbers

---
+++ CSharp_HW3_Task3_TableCubeNum/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+Console.WriteLine("Введите N. Целое, положительное.");
+int num = int.Parse(Console.ReadLine());
+double cub = 1;
+
+Console.WriteLine();
+
+if (num < 1)
+{
+    Console.WriteLine("Вы ввели число не удовлетворяющее условиям.");
+}
+else
+{
+    for (int i = 1; i < num + 1; i++)
+    {
+        cub = Math.Pow(i, 3);
+        Console.Write(cub + " ");
+    }
+}
+Console.WriteLine();
